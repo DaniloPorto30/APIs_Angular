79e8207158cfb3af977299a0f73825f078a0c2fa
DaniloPorto30 <dprosa23@gmail.com>
2021-07-21 16:00:11 -0300

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostsComponent,
+    FooterComponent,
+    CommentsComponent,
+    AlbumsComponent,
+    PhotosComponent,
+    TodosComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
